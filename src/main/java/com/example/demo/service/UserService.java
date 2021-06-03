24368635ac504a268b653a6f7e4156f2246a47a2
Premmulage <premasagarmulage@gmail.com>
2021-06-03 13:31:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entities.User;
+import com.example.demo.repos.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> listAllUser() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
